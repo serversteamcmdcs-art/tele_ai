7972b3c75dac7ed8ba6a722498764cc91ff64c0f
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-02-22 02:52:18 +0500

    Update vite.config.js

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -4,14 +4,10 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     port: 5173,
     host: '127.0.0.1',
     strictPort: false,
     allowedHosts: ['tele-ai-1.onrender.com']
+  }
 });
